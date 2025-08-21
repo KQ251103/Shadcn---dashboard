b8e6469536ff89c7fc4de87b4d8d9722d9ce7eaf
isabel <olazabalisabel53@gmail.com>
2025-08-21 00:22:45 -0500

    futuroForm

--- back-end/server.js
+++ back-end/server.js
@@ -7,6 +7,7 @@ import cors from 'cors';
 import personRoutes from './routes/personRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
 import emailRoutes from './routes/emailRoutes.js';
+import formRoutes from './routes/formRoutes.js';
 
 config(); // carga las variables de entorno
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use('/api/email', emailRoutes);
 app.use('/api/personas', personRoutes);
 app.use('/api/projects', projectRoutes);
+app.use('/api/form', formRoutes);
 
 // Ruta de prueba
 app.get('/', (req, res) => {
