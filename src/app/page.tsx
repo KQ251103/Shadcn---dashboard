36b67e157b0be82c55d25eb54102b7629703f262
isabel <olazabalisabel53@gmail.com>
2025-08-21 22:39:41 -0500

    login  interfaz-lista

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,34 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.xd}>Holaaaaaa
-    <h1 className={styles.xddd}>pero porque</h1>
     
-    </div>
+      <div className={styles.bodyy}>   
+     <div className={styles.login}>
+    <h2 className={styles.h2}>Login</h2>
+    <form action="#" className={styles.form}>
+
+      <div className={styles.inputt}>
+        <input type="email"  placeholder=" "  required minLength={3} maxLength={20} className={styles.inputField}/>
+        <label className={styles.label}>Email</label>
+        </div>
+
+        <div className={styles.inputt}>
+        <input type="password" required minLength={3} maxLength={20}  placeholder=" " className={styles.inputField}/>
+        <label  className={styles.label}>Password</label>
+      </div>
+
+        <button type="submit" className={styles.button}>Sing In</button>
+
+      
+    </form>
+    
+    </div> 
+    
+   </div>
+    
+
+   
+   
+   
   );
 }
